7d83e374eb72642b2f3b789a945a7c10cf7f4c29
Mario Miramontes <mmisthree@gmail.com>
2019-04-21 18:41:43 -0700

    created folder starting folder structure. installed axios, bootstrap

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="jumbotron text-center">
+                    <h1>Reciper Searching Machine</h1>
+                </div>
+            </div>
+        )
+    }
+};
+
+export default Header;
